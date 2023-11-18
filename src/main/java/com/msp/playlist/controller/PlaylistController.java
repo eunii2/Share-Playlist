f4d37bc7faca109c4d47451b8fef20172aba7541
GaeunChoi <chb7562@gmail.com>
2023-11-18 19:28:42 +0900

    first commit

--- src/main/java/com/msp/playlist/controller/PlaylistController.java
+++ src/main/java/com/msp/playlist/controller/PlaylistController.java
@@ -3,7 +3,6 @@ package com.msp.playlist.controller;
 import com.msp.playlist.dto.PlaylistRequestDto;
 import com.msp.playlist.entity.Playlist;
 import com.msp.playlist.service.PlaylistService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/playlist")
 public class PlaylistController {
-    @Autowired
-    private PlaylistService playlistService;
+
+    private final PlaylistService playlistService;
+
+    public PlaylistController(PlaylistService playlistService) {
+        this.playlistService = playlistService;
+    }
 
     @PostMapping
     public ResponseEntity<Playlist> createPlaylist(@RequestBody PlaylistRequestDto playlistRequestDto){
--- src/main/java/com/msp/playlist/service/PlaylistService.java
+++ src/main/java/com/msp/playlist/service/PlaylistService.java
@@ -3,15 +3,17 @@ package com.msp.playlist.service;
 import com.msp.playlist.dto.PlaylistRequestDto;
 import com.msp.playlist.entity.Playlist;
 import com.msp.playlist.repository.PlaylistRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
 public class PlaylistService {
-    @Autowired
-    private PlaylistRepository playlistRepository;
+    private final PlaylistRepository playlistRepository;
+
+    public PlaylistService(PlaylistRepository playlistRepository) {
+        this.playlistRepository = playlistRepository;
+    }
 
     public Playlist createPlaylist(PlaylistRequestDto playlistRequestDto){
         Playlist playlist = new Playlist();
